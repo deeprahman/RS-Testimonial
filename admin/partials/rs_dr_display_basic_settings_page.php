6d3ac031cd8fe86ee898afb144cff1e3f1f51569
Deep Rahman <dp.rahman@gmail.com>
2019-01-15 18:36:52 +0600

    Basic Settings with tabs working

--- admin/partials/rs_dr_display_basic_settings_page.php
+++ admin/partials/rs_dr_display_basic_settings_page.php
@@ -23,35 +23,39 @@ $cache_tab = add_query_arg(
     $active_tab = isset($_GET['tab']) ? $_GET['tab'] : 'review-tab';
     ?>
     <h2 class="nav-tab-wrapper">
+
         <a href="<?= $basic_tab ?>" class="nav-tab <?= $active_tab === 'basic-tab' ? 'nav-tab-active' : '' ?>">Basic
             Options</a>
         <a href="<?= $review_tab ?>" class="nav-tab <?= $active_tab === 'review-tab' ? 'nav-tab-active' : '' ?>">Review
             Options</a>
         <a href="<?= $cache_tab ?>" class="nav-tab <?= $active_tab === 'cache-tab' ? 'nav-tab-active' : '' ?>">Cache
             Options</a>
+
     </h2>
-    <form method="post" action="options.php">
-        <?php
-        switch ($active_tab) {
-            case 'review-tab':
-                settings_fields('rs_dr_testimonial_reviewed_settings');
-                do_settings_sections('rs-dr-testimonial-reviewed-option-page');
-                submit_button();
-                break;
+    <div class="from">
+        <form method="post" action="options.php">
+            <?php
+            switch ($active_tab) {
+                case 'review-tab':
+                    settings_fields('rs_dr_testimonial_reviewed_settings');
+                    do_settings_sections('rs-dr-testimonial-reviewed-option-page');
+                    submit_button();
+                    break;
 
-            case 'cache-tab':
-                settings_fields('rs_dr_testimonial_cache_settings');
-                do_settings_sections('rs-dr-testimonial-cache-option-page');
-                submit_button();
-                break;
+                case 'cache-tab':
+                    settings_fields('rs_dr_testimonial_cache_settings');
+                    do_settings_sections('rs-dr-testimonial-cache-option-page');
+                    submit_button();
+                    break;
 
-            default:
-                settings_fields('rs_dr_testimonial_basic_settings');
-                do_settings_sections('rs-dr-testimonial-basic-option-page');
-                submit_button();
-                break;
-        }
+                default:
+                    settings_fields('rs_dr_testimonial_basic_settings');
+                    do_settings_sections('rs-dr-testimonial-basic-option-page');
+                    submit_button();
+                    break;
+            }
 
-        ?>
-    </form>
+            ?>
+        </form>
+    </div>
 </div>
